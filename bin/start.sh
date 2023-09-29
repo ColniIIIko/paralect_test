b3fc3eadc17ebe9ab5a8a3a349c8800695bd8cab
ColniIIIko <isavitskiy12@gmail.com>
2023-09-29 21:08:54 +0300

    ref: remove mailer service

--- bin/start.sh
+++ bin/start.sh
@@ -5,7 +5,7 @@ Green='\033[0;32m'
 Color_Off='\033[0m'
 
 echo 'You can start services independently'
-echo $Green'./bin/start.sh api migrator scheduler web mailer'
+echo $Green'./bin/start.sh api migrator scheduler web'
 
 echo $Color_Off
 docker-compose up --build "$@"
